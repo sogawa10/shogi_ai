6beecfe9d4d46e9a857b0bb30a1cb4bc84dae947
Soki-1010 <sogawa.doshisha@gmail.com>
2026-02-15 22:09:17 +0900

    手の表示を変更

--- shogi_ai/対局用/手.py
+++ shogi_ai/対局用/手.py
@@ -10,16 +10,14 @@ class 手:
     komadori: object | None = None
 
     def to_string(self, player_sente_or_gote):
-        int2kanji_map = {
-            0: '一', 1: '二', 2: '三', 3: '四', 4: '五', 5: '六', 6: '七', 7: '八', 8: '九'
-        }
-        int2zenkaku_map = {
-            0: '１', 1: '２', 2: '３', 3: '４', 4: '５', 5: '６', 6: '７', 7: '８', 8: '９'
-        }
+        if self.koma.sente_or_gote() == "先手":
+            turn = "▲"
+        else:
+            turn = "△"
         if self.uchite:
-            return (int2zenkaku_map[self.to_pos[0]] + int2kanji_map[self.to_pos[1]] + self.koma.symbol(player_sente_or_gote)[1] + "打")
+            return (turn + str(self.to_pos[0] + 1) + str(self.to_pos[1] + 1) + self.koma.symbol(player_sente_or_gote)[1] + "打")
         else:
             if self.nari:
-                return (int2zenkaku_map[self.to_pos[0]] + int2kanji_map[self.to_pos[1]] + self.koma.symbol(player_sente_or_gote)[1] + "成 （" + int2zenkaku_map[self.from_pos[0]] + int2kanji_map[self.from_pos[1]] + "）")
+                return (turn + str(self.to_pos[0] + 1) + str(self.to_pos[1] + 1) + self.koma.symbol(player_sente_or_gote)[1] + "成(" + str(self.from_pos[0] + 1) + str(self.from_pos[1] + 1) + ")")
             else:
-                return (int2zenkaku_map[self.to_pos[0]] + int2kanji_map[self.to_pos[1]] + self.koma.symbol(player_sente_or_gote)[1] + " （" + int2zenkaku_map[self.from_pos[0]] + int2kanji_map[self.from_pos[1]] + "）")
+                return (turn + str(self.to_pos[0] + 1) + str(self.to_pos[1] + 1) + self.koma.symbol(player_sente_or_gote)[1] + "(" + str(self.from_pos[0] + 1) + str(self.from_pos[1] + 1) + ")")
